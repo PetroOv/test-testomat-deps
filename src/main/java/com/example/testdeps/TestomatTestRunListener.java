b0d440d8e4be04d46d58fd643e11bcb405d21c2a
petroov <petro.ovcharenko@solidgate.com>
2025-10-06 15:50:29 +0300

    Initial commit

---
+++ src/main/java/com/example/testdeps/TestomatTestRunListener.java
@@ -0,0 +1,37 @@
+package com.example.testdeps;
+
+import io.testomat.core.constants.PropertyNameConstants;
+import io.testomat.testng.listener.TestNgListener;
+import java.util.ArrayList;
+import java.util.List;
+import org.testng.ISuite;
+
+public class TestomatTestRunListener extends TestNgListener {
+
+  private static final String JENKINS_JOB_PREFIX = "https://jenkins.solidgate-dev.com/job/aqa/job/";
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+
+  @Override
+  public void onStart(ISuite suite) {
+    System.setProperty("testomatio.listening", isEnabled() ? "true" : null);
+
+    var runName = "some";
+      System.setProperty(PropertyNameConstants.RUN_TITLE_PROPERTY_NAME, runName);
+      System.setProperty(PropertyNameConstants.RUN_GROUP_PROPERTY_NAME, runName.split("/")[0]);
+    var tags = createTestRunTags();
+    var tagsString = tags != null ? " @" + String.join(" @", tags) : "";
+    System.setProperty(PropertyNameConstants.RUN_TITLE_PROPERTY_NAME, runName);
+    System.setProperty(PropertyNameConstants.ENVIRONMENT_PROPERTY_NAME, "prod");
+    super.onStart(suite);
+  }
+
+  private List<String> createTestRunTags() {
+    List<String> tags = new ArrayList<>();
+    return tags.isEmpty() ? null : tags;
+  }
+
+}
